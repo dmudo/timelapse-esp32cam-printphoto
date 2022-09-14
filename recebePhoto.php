338731d7ba72dd78fab6ec864b870f8687df68ad
D'angelo Martins <dangelomartins@gmail.com>
2022-09-13 21:27:59 -0300

    WORK pronto p/ 1 img

--- recebePhoto.php
+++ recebePhoto.php
@@ -1,30 +1,32 @@
 <?php
-
+//Lê o conteudo enviado pela EspCam
 $received = file_get_contents('php://input');
-$fileToWrite = "uploads/newphoto.jpg";
+$fileToWrite = "uploads/recebidaESPCam.jpg";
 file_put_contents($fileToWrite, $received);
 $im = imagecreatefromjpeg($fileToWrite);
 
-$white = imagecolorallocate($im, 255, 255, 255);
+//Colorir texto
+$white = imagecolorallocate($im, 199, 196, 15);
 $black = imagecolorallocate($im, 0, 12, 0);
 
+// Fontes para usar na escrita
 $font = dirname(__FILE__) . "/SegoeBold.ttf";
 $font1 = dirname(__FILE__) . "/segoe.otf";
 
-// $nome = $_POST['nome'];
+// Escreve o nome da cidade
 $nome = 'Goiânia';
-// $celular = urldecode( $_POST['celular'] );
 
-// temperatura = $_POST['departamento0'];
+// Escreve Temperatura e Umidade 
 $temperatura = 'Temperatura: 35º';
 $Umidade = 'Umidade: 70%';
 
+//Posiçoes dos textos na imagem
+imagettftext($im, 55, 0, 30, 1043, $white, $font, $nome );
+imagettftext($im, 30, 0, 30, 1084, $white, $font1, $temperatura );
+imagettftext($im, 30, 0, 30, 1122, $white, $font1, $Umidade );
 
-imagettftext($im, 35, 0, 30, 43, $white, $font, $nome );
-imagettftext($im, 15, 0, 30, 92, $white, $font1, $temperatura );
-imagettftext($im, 15, 0, 30, 122, $white, $font1, $Umidade );
 
-
-// Envia a imagem para o borwser ou arquivo
-imagejpeg( $im, 'uploads/'.time().'.jpg', 100 );
+// Envia a imagem para o browser ou arquivo
+imagejpeg( $im, 'uploads/pronta'.'.jpg', 100 );
 imagedestroy($fileToWrite);
+
